fc59efc38ad22694774a31f652c782d66a69096e
Mohammad Amin Taheri <taherima999@gmail.com>
2020-12-31 00:54:06 +0330

    Add some war screen images & texts, add victory scenes for player 1&2 and flashback to the war screen scene

---
+++ Assets/Scripts/SceneManager/VictorySceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictorySceneSwitcher : MonoBehaviour
+{
+    public int sceneID;
+
+    public void SceneSwitcher(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
