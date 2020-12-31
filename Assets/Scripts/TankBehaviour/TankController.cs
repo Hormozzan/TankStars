e206f06be63e9cf87a7a5106bcbcb03f1da4a9a1
alirezaSafi <al02reza78@gmail.com>
2020-12-31 18:42:30 +0330

    Add tank controller script

---
+++ Assets/Scripts/TankBehaviour/TankController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public GameObject Tank1;
+    public GameObject Tank2;
+
+    void Start()
+    {
+        Tank1.GetComponent<Movement>().turn = 1;
+        Tank2.GetComponent<Movement>().turn = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Tank1.GetComponent<Movement>().turn = 1 - Tank1.GetComponent<Movement>().turn;
+            Tank2.GetComponent<Movement>().turn = 1 - Tank2.GetComponent<Movement>().turn;
+
+        }
+        
+    }
+}
